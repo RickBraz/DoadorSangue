79daf4ce4e36dff0a335732f2c79f2f35c9a4bcc
= <=>
2020-06-26 19:17:55 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.Write("Digite sua idade para saber se voce pode doar sangue: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade >= 17 || idade <= 68)
+            {
+                Console.WriteLine("Voce pode ser doador de sangue");
+            }
+            else 
+            {
+                Console.WriteLine("Voce nao pode ser doador de sangue");
+            }
+        }
+    }
+}
